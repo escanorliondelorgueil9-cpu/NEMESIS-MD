868d279e630a14e60af76a182cab301f679d6446
escanorliondelorgueil9-cpu <escanorliondelorgueil9@gmail.com>
2026-05-30 16:33:57 +0100

    Crear messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "Digix Crew",
+        body: "𝗡𝗘𝗠𝗘𝗦𝗜𝗦-𝗠𝗗",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
